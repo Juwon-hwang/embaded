239029bda7c99754e288dbbf318f54d4f401ea67
juwon <juwon2588@naver.com>
2023-03-16 14:04:01 +0900

    hi

--- sketch_mar14a.ino
+++ sketch_mar14a.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+const int LED_PIN = 12;  // LED가 연결된 핀 번호
+bool led_state = true;   // LED의 초기 상태는 켜짐으로 설정
 
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // 스페이스바가 눌리면 LED를 끄거나 켭니다.
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == ' ') {
+      led_state = !led_state;
+      digitalWrite(LED_PIN, led_state);
+      Serial.println(led_state ? "LED ON" : "LED OFF");
+    }
+  }
 }
